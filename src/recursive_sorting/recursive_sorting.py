26b2ab12166cbb0933472e840e67f250679dbf3e
davidanagy <davidanagy@users.noreply.github.com>
2020-03-17 16:09:55 -0700

    completed in-place merge sort; started timsort

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -66,17 +66,68 @@ def merge_sort( arr ):
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
     # TO-DO
+    arrA = arr[start:mid]
+    arrB = arr[mid:end]
+    for i in range(end-start):
+        if len(arrA) == 0:
+            lowest_B = arrB[0]
+            arr[start+i] = lowest_B
+            arrB.remove(lowest_B)
+        elif len(arrB) == 0:
+            lowest_A = arrA[0]
+            arr[start+i] = lowest_A
+            arrA.remove(lowest_A)
+        else:
+            lowest_A = arrA[0]
+            lowest_B = arrB[0]
+            if lowest_A <= lowest_B:
+                arr[start+i] = lowest_A
+                arrA.remove(lowest_A)
+            else:
+                arr[start+i] = lowest_B
+                arrB.remove(lowest_B)
 
     return arr
 
 def merge_sort_in_place(arr, l, r): 
     # TO-DO
+    if r - l <= 0:
+        pass
+    else:
+        mid = (l + r + 1) // 2
+        #print('begin:', arr, l, mid, r)
+        if r - l == 1:
+            arr = merge_in_place(arr, l, mid, r+1)
+            #print('merge in place:', arr)
+        else:
+            arr = merge_sort_in_place(arr, l, mid-1)
+            #print('merge_sort 1:', arr)
+            arr = merge_sort_in_place(arr, mid, r)
+            #print('merge_sort 2:', arr)
+            arr = merge_in_place(arr, l, mid, r+1)
+            #print('merge_in_place:', arr)
 
     return arr
 
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
+# def timsort( arr ):
+#     num_runs = len(arr) // 32
+#     runs = []
+#     for i in range(num_runs):
+#         runs.append(arr[i:i+32])
 
-    return arr
+#     def insertion_sort(arr):
+#         first_element = arr[0]
+#         for i in range(1, len(arr)):
+#             element = arr[i]
+#             for j in reversed(range(0, i)):
+#                 if j > i:
+#                     bigger_element = arr[j]
+#                     arr[j+1] = bigger_element
+#                 else:
+#                     arr[j+1] = element
+#                     break
+
+#     return arr
